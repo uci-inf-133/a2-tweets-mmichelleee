0d14585e73666a86aec720a7c7998251ff58923f
Michelle Mateo <mmateo3@uci.edu>
2025-10-25 19:28:54 -0700

    add tweetdates in about.js

--- js/about.js
+++ js/about.js
@@ -12,6 +12,18 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+
+    const tweetDates = tweet_array.map(t => t.time); // gathers the dates
+
+    const earliest = new Date(Math.min(...tweetDates)); 
+    const latest = new Date(Math.max(...tweetDates));
+
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }; //turns the dates into strings
+    const earliestFormatted = earliest.toLocaleDateString('en-US', options);
+    const latestFormatted = latest.toLocaleDateString('en-US', options);
+
+    document.getElementById('firstDate').innerText = earliestFormatted; // span
+    document.getElementById('lastDate').innerText = latestFormatted; // span
 }
 
 //Wait for the DOM to load
